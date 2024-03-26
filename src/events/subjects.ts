538646a8dbcfe521f4fe04c0ae6f7a1487fb945e
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2024-03-26 22:45:41 +0000

    Updates

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -3,6 +3,7 @@ export enum Subjects {
   TicketUpdated = "ticket:updated",
 
   OrderCreated = "order:created",
+  OrderUpdated = "order:updated",
   OrderCancelled = "order:cancelled",
 
   ExpirationComplete = "expiration:complete",
