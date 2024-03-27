8049b0b7695f621ad038013b94260b04cb33ec32
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2024-03-27 07:49:42 +0000

    Remove build

--- build/events/subjects.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum Subjects {
-    TicketCreated = "ticket:created",
-    TicketUpdated = "ticket:updated",
-    OrderCreated = "order:created",
-    OrderCancelled = "order:cancelled",
-    OrderUpdated = "order:updated",
-    ExpirationComplete = "expiration:complete",
-    PaymentCreated = "payment:created"
-}
